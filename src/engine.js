5548ed382466da17c60ffb66d1f03e944ce97aee
York Wong <york.wong@objective.com>
2021-08-05 13:35:47 +1000

    initial commit

---
+++ src/engine.js
@@ -0,0 +1,8 @@
+// (world: boolean[][]) => boolean[][]
+export const next = (world) => {
+  return world;
+};
+// (pattern: string) => boolean[][]
+export const parse = (pattern) => {
+  return [];
+};
