07c01cecfa7031de14fc9c08c994a0bb62cac972
samipnepal <samip_nepal@yahoo.com>
2022-03-11 08:55:58 +0800

    Added more test cases

--- src/engine.test.js
+++ src/engine.test.js
@@ -17,7 +17,7 @@ describe("next", () => {
     ]);
   });
 
-  it.skip("should be dead if there is no neighbours", () => {
+  it("should be dead if there is no neighbours", () => {
     const result = next([
       [_, _, _],
       [_, o, _],
@@ -25,13 +25,34 @@ describe("next", () => {
     ]);
     expect(result[1][1]).to.equal(false);
   });
+
+  it("should be dead if there is no neighbours for more than one active cells", () => {
+    const result = next([
+      [_, _, _, _],
+      [_, o, _, o],
+      [_, _, _, _],
+    ]);
+    expect(result[1][1]).to.equal(false);
+    expect(result[1][3]).to.equal(false);
+  });
+
+  it("should revive if there is 3 neighbours", () => {
+    const result = next([
+      [_, _, _, _],
+      [_, o, _, o],
+      [_, _, o, _],
+    ]);
+    expect(result[1][1]).to.equal(false);
+    expect(result[1][3]).to.equal(false);
+    expect(result[1][2]).to.equal(true);
+  });
 });
 
 describe("parse", () => {
   it("should return [] given ''", () => {
     expect(parse("")).to.deep.equal([]);
   });
-  it.skip("should parse O as true and . as false", () => {
+  it("should parse O as true and . as false", () => {
     expect(parse("...\n.O.\n...\n")).to.deep.equal([
       [_, _, _],
       [_, o, _],
